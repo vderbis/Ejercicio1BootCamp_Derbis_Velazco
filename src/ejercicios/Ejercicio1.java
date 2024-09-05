feb706d3d942a06065869e92bc016d295a905a39
DERBIS <derbis.velazco@tsoftglobal.com>
2024-09-05 14:32:43 -0400

    Se crea ejercicio1 de Ejercicios de repaso Java

---
+++ src/ejercicios/Ejercicio1.java
@@ -0,0 +1,20 @@
+package ejercicios;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        /*
+        * Suma de dos numero
+        * a = 10 y b=20
+        * Descripción: Dados los números a = 10 y b = 20, calcula la suma de ambos y muestra el resultado.
+         */
+        int a=10;
+        int b=20;
+        int t;
+        t=a+b;
+
+        System.out.println("la suma de "+a+" + "+b+" da como resultado: "+t);
+        System.out.println("Derbis Velazco");
+    }
+
+
+}
