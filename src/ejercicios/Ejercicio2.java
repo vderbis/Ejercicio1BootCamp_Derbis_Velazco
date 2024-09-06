9b4ad610b7856eee7eeda0ced34b72db88e8d07e
DERBIS <derbis.velazco@tsoftglobal.com>
2024-09-06 12:14:35 -0400

    Se crea ejercicio2 de Ejercicios de repaso Java

---
+++ src/ejercicios/Ejercicio2.java
@@ -0,0 +1,19 @@
+package ejercicios;
+
+public class Ejercicio2 {
+    public static void main(String[] args)
+    {
+        /*
+         * Suma de dos numero
+         * a = 10 y b=20
+         * Descripción: Dados los números a = 10 y b = 20, calcula la suma de ambos y muestra el resultado.
+         */
+        int a=7;
+        int b=8;
+        int p;
+        p=a*b;
+
+        System.out.println("la Multiplicacion de "+a+" * "+b+" da como resultado: "+p);
+        System.out.println("Derbis Velazco");
+    }
+}
