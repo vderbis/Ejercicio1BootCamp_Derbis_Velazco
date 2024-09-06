779d213d3cf7ab4b8979936256aaf471a0d1086f
DERBIS <derbis.velazco@tsoftglobal.com>
2024-09-06 12:17:11 -0400

    Se crea ejercicio2 de Ejercicios de repaso Java (Correccion)

--- src/ejercicios/Ejercicio2.java
+++ src/ejercicios/Ejercicio2.java
@@ -4,9 +4,9 @@ public class Ejercicio2 {
     public static void main(String[] args)
     {
         /*
-         * Suma de dos numero
-         * a = 10 y b=20
-         * Descripción: Dados los números a = 10 y b = 20, calcula la suma de ambos y muestra el resultado.
+         * Producto de dos numero
+         * a = 7 y b=8
+         * Descripción: Dados los números a = 7 y b = 8, calcula el producto de ambos y muestra el resultado.
          */
         int a=7;
         int b=8;
