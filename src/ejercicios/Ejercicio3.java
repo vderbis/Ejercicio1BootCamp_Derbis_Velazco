e74b0fab64f701845069bc3d074e1c0dc209da83
DERBIS <derbis.velazco@tsoftglobal.com>
2024-09-06 14:05:08 -0400

    Se crea ejercicio3 de Ejercicios de repaso Java

---
+++ src/ejercicios/Ejercicio3.java
@@ -0,0 +1,18 @@
+package ejercicios;
+     public class Ejercicio3 {
+     /*
+     * Número par o impar
+     * a = 15
+     * Descripción: Dado el número a = 15, determina si es par o impar. Un número es par si es divisible
+     */
+    public static void main(String[] args)   {
+        int a = 15;
+        if (a % 2 == 0)   {
+            System.out.println(a + " El numero es Par");
+        } else {
+            System.out.println(a + " El numero es Impar");
+        }
+        System.out.println("Derbis Velazco");
+    }
+     }
+
