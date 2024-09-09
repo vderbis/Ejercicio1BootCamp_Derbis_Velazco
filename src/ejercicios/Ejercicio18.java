674e994f042595a6bbfe687a87de4bfb1d0175b1
DERBIS <derbis.velazco@tsoftglobal.com>
2024-09-09 15:26:52 -0300

    Se crea ejercicio18 de Ejercicios de repaso Java

---
+++ src/ejercicios/Ejercicio18.java
@@ -0,0 +1,12 @@
+package ejercicios;
+public class Ejercicio18 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 8;
+        // Calcula 2 elevado a la potencia de 8
+        double resultado = Math.pow(a, b);
+        // Imprime el resultado
+        System.out.println("El resultado de " + a + " elevado a la potencia de " + b + " es: " + (int)resultado);
+        System.out.println("Derbis Velazco");
+    }
+}
