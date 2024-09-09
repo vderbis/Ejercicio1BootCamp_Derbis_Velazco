c92a1d3ef8e3f038471ac269d569d9716db60c0c
DERBIS <derbis.velazco@tsoftglobal.com>
2024-09-09 16:31:34 -0300

    Se crea ejercicio26 de Ejercicios de repaso Java

---
+++ src/ejercicios/Ejercicio26.java
@@ -0,0 +1,10 @@
+package ejercicios;
+
+public class Ejercicio26 {
+    public static void main(String[] args) {
+        int numero = -15;
+        int valorAbsoluto = Math.abs(numero);
+        System.out.println("El valor absoluto de " + numero + " es: " + valorAbsoluto);
+        System.out.println("Derbis Velazco");
+    }
+}
