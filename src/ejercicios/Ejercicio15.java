5bd394c63c2b4bbba02fbfd4a0800fd52f3e8154
DERBIS <derbis.velazco@tsoftglobal.com>
2024-09-09 14:50:57 -0300

    Se crea ejercicio15 de Ejercicios de repaso Java

---
+++ src/ejercicios/Ejercicio15.java
@@ -0,0 +1,10 @@
+package ejercicios;
+
+public class Ejercicio15 {
+    public static void main(String[] args) {
+        double r = 7; // Radio del círculo
+        double area = Math.PI * Math.pow(r, 2); // Fórmula para el área
+        System.out.println("El área del círculo es: " + area);
+        System.out.println("Derbis Velazco");
+    }
+}
